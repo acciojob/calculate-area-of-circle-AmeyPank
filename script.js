91b4462c3d5f684974ba2a73c36142e9f239e498
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-07-11 14:11:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+var radius = parseFloat(prompt("Enter the radius of the circle:"));
+var area = Math.PI * Math.pow(radius, 2);
+area = area.toFixed(2);
+
+alert("The area of the circle with radius " + radius + " is " + area);
